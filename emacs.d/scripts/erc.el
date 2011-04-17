3c23acee2e5cfd148cf4661cfb95af629487a631
Hans Engel <engel@engel.uk.to>
2011-04-17 11:27:18 -0700

    a few logging changes

--- emacs.d/scripts/erc.el
+++ emacs.d/scripts/erc.el
@@ -17,7 +17,13 @@
 (require 'erc-log)
 (setq erc-log-channels t)
 (setq erc-log-channels-directory "~/.irclogs")
+(setq erc-save-buffer-on-part t) ;; automatically write to log files when parting channels
 (setq erc-hide-timestamps nil)
+
+;; auto-save log files on emacs exit
+(defadvice save-buffers-kill-emacs (before save-logs (arg) activate)
+  (save-some-buffers t (lambda () (when (and (eq major-mode 'erc-mode)
+                                             (not null buffer-file-name))))))
 
 ;; by default, when you join a channel ERC fills the screen with previous messages from a log for that channel if one exists.
 ;; the following code disables that functionality. see http://www.emacswiki.org/emacs/ErcLogging#toc2
