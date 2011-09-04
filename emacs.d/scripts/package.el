91676c37e569eb066aeda69876567fb6822f1297
Hans Engel <engel@engel.uk.to>
2011-09-04 16:52:42 -0700

    Package config

---
+++ emacs.d/scripts/package.el
@@ -0,0 +1,7 @@
+; Package management
+
+(add-to-list 'package-archives '("elpa" .
+                                 "http://tromey.com/elpa/"))
+
+(add-to-list 'package-archives '("marmalade" .
+                                 "http://marmalade-repo.org/packages/"))
