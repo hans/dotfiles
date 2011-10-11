c9c98f2fe57f5523dba325551acae220cd37d83d
Hans Engel <engel@engel.uk.to>
2011-10-11 12:36:21 -0700

    added clojure-mode customizations

---
+++ emacs.d/scripts/clojure-mode.el
@@ -0,0 +1,6 @@
+;; clojure-mode
+
+(setq swank-clojure-jar-path "~/.clojure/clojure.jar"
+      swank-clojure-extra-classpaths (list
+                                      "~/.clojure/clojure-contrib.jar"
+                                      "~/.clojure/src/clj/clojure"))
