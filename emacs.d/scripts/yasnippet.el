86c61fd127df12ffb61c30eddf558e772ba05add
Hans Engel <engel@engel.uk.to>
2011-10-13 11:01:39 -0700

    emacs: yasnippet: I like dropdowns better :)

--- emacs.d/scripts/yasnippet.el
+++ emacs.d/scripts/yasnippet.el
@@ -4,16 +4,11 @@
 
 (yas/global-mode 1)
 
-(yas/load-directory "~/.emacs.d/vendor/yasnippet/snippets")
-;(yas/load-directory "~/.emacs.d/vendor/yasnippets-rails/rails-snippets")
-;(yas/load-directory "~/.emacs.d/vendor/yasnippets-shoulda")
-
-(setq yas/prompt-functions '(yas/ido-prompt
-                             yas/dropdown-prompt
+(setq yas/prompt-functions '(yas/dropdown-prompt
+                             yas/ido-prompt
                              yas/completing-prompt))
 
 ;; Replace yasnippets's TAB
 (add-hook 'yas/minor-mode-hook
           (lambda () (define-key yas/minor-mode-map
                        (kbd "TAB") 'smart-tab))) ; was yas/expand
-
