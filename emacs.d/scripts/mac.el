2f520f2922ef171258d777538a73a40795373cdc
Hans Engel <engel@engel.uk.to>
2011-10-14 09:43:31 -0700

    remove copy/paste hack.. not necessary now that we're using Cocoa Emacs

--- emacs.d/scripts/mac.el
+++ emacs.d/scripts/mac.el
@@ -8,19 +8,6 @@
 (when (boundp 'mouse-wheel-scroll-amount)
   (setq mouse-wheel-scroll-amount '(0.01)))
 
-;; Copy and paste to Mac clipboard when copying / yanking
-;; http://www.lingotrek.com/2010/12/integrate-emacs-with-mac-os-x-clipboard.html
-(defun mac-copy ()
-  (shell-command-to-string "pbpaste"))
-(defun mac-paste (text &optional push)
-  (let ((process-connection-type nil))
-    (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
-      (process-send-string proc text)
-      (process-send-eof proc))))
-
-(setq interprogram-cut-function 'mac-paste)
-(setq interprogram-paste-function 'mac-copy)
-
 ;; Cocoa Emacs: M-RET to toggle fullscreen
 (when (fboundp 'ns-toggle-fullscreen)
   (global-set-key (kbd "M-RET") 'ns-toggle-fullscreen))
