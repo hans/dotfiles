1cf5c6e629d09356f757cfea63cb2d70da1d8b82
Hans Engel <engel@engel.uk.to>
2011-10-28 19:44:34 -0700

    Add CSS Flymake parsing + a bit of reorganization

--- emacs.d/scripts/flymake.el
+++ emacs.d/scripts/flymake.el
@@ -75,21 +75,50 @@
                        (file-name-directory buffer-file-name))))
     (list "/usr/local/bin/jslint" (list local-file))))
 
-(setq flymake-allowed-file-name-masks
-      (cons '(".+\\.js$"
-              flymake-jslint-init
-              flymake-simple-cleanup
-              flymake-get-real-file-name)
-            flymake-allowed-file-name-masks))
 
-(setq flymake-err-line-patterns
-      (cons '("[[:digit:]] \\([[:digit:]]+\\),\\([[:digit:]]+\\): \\(.+\\)" nil 1 2 3)
-            flymake-err-line-patterns))
 
 (provide 'flymake-jslint)
 (require 'flymake-jslint)
 (add-hook 'js-mode-hook (lambda () (flymake-mode 1)))
 
+;;; CSS
+;;; Uses the Python cssutils module. To install:
+;;;
+;;;     easy_install cssutils
+
+(defun flymake-css-init ()
+  (let* ((temp-file (flymake-init-create-temp-buffer-copy
+                     'flymake-create-temp-inplace))
+         (local-file (file-relative-name
+                      temp-file
+                      (file-name-directory buffer-file-name))))
+    (list "cssparse" (list local-file))))
+
+(add-hook 'css-mode-hook (lambda () (flymake-mode t)))
+
+(setq flymake-allowed-file-name-masks
+      (cons
+       ;; JS
+       '(".+\\.js$"
+         flymake-jslint-init
+         flymake-simple-cleanup
+         flymake-get-real-file-name)
+
+       ;; CSS
+       (cons
+        '("\\.css$" flymake-css-init)
+        flymake-allowed-file-name-masks)))
+
+(setq flymake-err-line-patterns
+      (cons
+       ;; JS
+       '("[[:digit:]] \\([[:digit:]]+\\),\\([[:digit:]]+\\): \\(.+\\)" nil 1 2 3)
+
+       ;; CSS
+       (cons
+        '("\\(.*\\) \\[\\([0-9]+\\):\\([0-9]+\\): \\(.*\\)\\]"
+          nil 2 3 1)
+        flymake-err-line-patterns)))
 
 ;; RFringe: Display number and location of flymake errors/warnings on the fringe
 ;; Load custom .el in .emacs.d/vendor
