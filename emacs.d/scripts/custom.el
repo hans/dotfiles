fd0e08b0a96e987ef9e12af1ba38fd0325ca3387
Hans Engel <engel@engel.uk.to>
2012-01-21 21:52:45 -0700

    emacs: Stop mumamo background highlighting. Yuck.

---
+++ emacs.d/scripts/custom.el
@@ -0,0 +1,16 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(mumamo-background-chunk-major ((t nil)))
+ '(mumamo-background-chunk-submode1 ((t nil)))
+ '(mumamo-background-chunk-submode2 ((t nil)))
+ '(mumamo-background-chunk-submode3 ((t nil)))
+ '(mumamo-background-chunk-submode4 ((t nil))))
