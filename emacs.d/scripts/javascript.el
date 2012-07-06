8a308521940a7b64ed94630cb0013377c6dcef60
Hans Engel <engel@engel.uk.to>
2012-07-06 10:40:45 -0700

    Durr.. wrong order

--- emacs.d/scripts/javascript.el
+++ emacs.d/scripts/javascript.el
@@ -1,10 +1,5 @@
 ;;; Javascript is fun
 
-(load "js2-mode/js2-mode")
-(load "js2-mode/js2-imenu-extras")
-(require 'js2-mode)
-(require 'js2-imenu-extras)
-
 (setq-default
  ;; Don't rebind C-a / C-e for me, thanks
  js2-rebind-eol-bol-keys nil
@@ -15,5 +10,10 @@
  js2-global-externs '("setTimeout" "clearTimeout" "setInterval"
                       "clearInterval" "location" "console" "_"
                       "jQuery" "$"))
+
+(load "js2-mode/js2-mode")
+(load "js2-mode/js2-imenu-extras")
+(require 'js2-mode)
+(require 'js2-imenu-extras)
 
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
