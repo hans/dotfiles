d7745784a4e75eb523c1dcfedf7a4ef573b37103
Hans Engel <engel@engel.uk.to>
2012-07-05 21:55:21 -0700

    Fullscreen magit-status + 'q' keybinding to restore previous window status. AWESOME.

--- emacs.d/scripts/magit.el
+++ emacs.d/scripts/magit.el
@@ -11,7 +11,23 @@
      (set-face-foreground 'magit-diff-add "green3")
      (set-face-foreground 'magit-diff-del "red3")
      (set-face-background 'magit-item-highlight "gray15")
+
      (define-key magit-mode-map (kbd "M-3") 'split-window-horizontally) ; was magit-show-level-3
      (define-key magit-mode-map (kbd "M-2") 'split-window-vertically)   ; was magit-show-level-2
      (define-key magit-mode-map (kbd "M-1") 'delete-other-windows)      ; was magit-show-level-1
+
+     ;; full screen magit-status
+
+     (defadvice magit-status (around magit-fullscreen activate)
+       (window-configuration-to-register :magit-fullscreen)
+       ad-do-it
+       (delete-other-windows))
+
+     (defun magit-quit-session ()
+       "Restores the previous window configuration and kills the magit buffer"
+       (interactive)
+       (kill-buffer)
+       (jump-to-register :magit-fullscreen))
+
+     (define-key magit-status-mode-map (kbd "q") 'magit-quit-session)
      ))
