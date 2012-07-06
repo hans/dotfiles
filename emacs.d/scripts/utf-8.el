6d0d790e923f26983ad901b591e60f57b2488f25
Hans Engel <engel@engel.uk.to>
2012-07-05 21:32:29 -0700

    Moar UTF8

--- emacs.d/scripts/utf-8.el
+++ emacs.d/scripts/utf-8.el
@@ -1,9 +1,13 @@
 ;;; Gotta do UTF-8
 
-(require 'un-define "un-define" t)
+;; How many encoding functions are there .. ?
+
+(setq-default locale-coding-system 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(set-selection-coding-system 'utf-8)
 (set-buffer-file-coding-system 'utf-8 'utf-8-unix)
 (set-default buffer-file-coding-system 'utf-8-unix)
 (set-default-coding-systems 'utf-8-unix)
 (prefer-coding-system 'utf-8-unix)
 (set-default default-buffer-file-coding-system 'utf-8-unix)
-
