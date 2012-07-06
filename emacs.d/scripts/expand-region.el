a692e097ecb0e0cbf3423a708798e7a307b02692
Hans Engel <engel@engel.uk.to>
2012-07-05 17:01:04 -0700

    New expand-region shortcuts

--- emacs.d/scripts/expand-region.el
+++ emacs.d/scripts/expand-region.el
@@ -1,2 +1,5 @@
 (require 'expand-region)
-(global-set-key (kbd "M-@") 'er/expand-region)
+
+(global-set-key (kbd "C-=") 'er/expand-region)
+(global-set-key (kbd "C-c q") 'er/mark-outside-quotes)
+(global-set-key (kbd "C-c Q") 'er/mark-inside-quotes)
