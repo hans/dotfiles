4ef380b5ce1a9c8880939ee977fda04d83d323ab
Hans Engel <engel@engel.uk.to>
2012-07-05 21:37:16 -0700

    Fontify org-mode code blocks

--- emacs.d/scripts/org.el
+++ emacs.d/scripts/org.el
@@ -9,6 +9,7 @@
 (setq org-directory "~/org/"
       org-default-notes-file (concat org-directory "notes.org")
       org-M-RET-may-split-line nil
-      org-todo-keywords '((sequence "TODO" "IN PROGRESS" "DONE")))
+      org-todo-keywords '((sequence "TODO" "IN PROGRESS" "DONE"))
+      org-src-fontify-natively t)
 
 (define-key global-map (kbd "C-c c") 'org-capture)
