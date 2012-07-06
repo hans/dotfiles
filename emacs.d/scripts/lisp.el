8e12b454aab85c1ce254a0f6ee59751cc82cf9f3
Hans Engel <engel@engel.uk.to>
2012-07-05 17:54:24 -0700

    Lisp updates: paredit in IELM; proper font-lock for Clojure in SLIME

--- emacs.d/scripts/lisp.el
+++ emacs.d/scripts/lisp.el
@@ -1,5 +1,6 @@
 ; Autoload Paredit for editing Lispy code
 (add-hook 'emacs-lisp-mode-hook (lambda () (paredit-mode +1)))
+(add-hook 'ielm-mode-hook (lambda () (paredit-mode +1)))
 (add-hook 'lisp-mode-hook (lambda () (paredit-mode +1)))
 (add-hook 'lisp-interaction-mode-hook (lambda () (paredit-mode +1)))
 (add-hook 'scheme-mode-hook (lambda () (paredit-mode +1)))
@@ -9,4 +10,5 @@
 ;; Slime
 
 ; Clojure syntax highlighting in REPL
+(require 'clojure-mode)
 (add-hook 'slime-repl-mode-hook 'clojure-mode-font-lock-setup)
