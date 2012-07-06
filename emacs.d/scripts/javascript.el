23b7aec99b9ad2469b145eff9942d8163aaa4aa8
Hans Engel <engel@engel.uk.to>
2012-07-05 17:22:20 -0700

    Include js2 imenu extras

--- emacs.d/scripts/javascript.el
+++ emacs.d/scripts/javascript.el
@@ -1,7 +1,9 @@
 ;;; Javascript is fun
 
 (load "js2-mode/js2-mode")
+(load "js2-mode/js2-imenu-extras")
 (require 'js2-mode)
+(require 'js2-imenu-extras)
 
 (setq-default js2-allow-rhino-new-expr-initializer nil
               js2-global-externs '("setTimeout" "clearTimeout" "setInterval"
