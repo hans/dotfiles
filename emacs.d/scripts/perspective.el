7b14e9ce3c64fcae3e27d1872975e419d383411e
Hans Engel <engel@engel.uk.to>
2012-07-07 21:22:28 -0700

    More defpersp

--- emacs.d/scripts/perspective.el
+++ emacs.d/scripts/perspective.el
@@ -1,24 +1,40 @@
 (require 'perspective)
 (persp-mode 1)
 
-(defmacro defpersp (name path-match key &rest body)
-  "Define a perspective with a name NAME which loads when a file that matches
-PATH-MATCH is opened. Set up a keybinding to load the perspective manually by
-pressing `C-x p KEY`. Run BODY when the perspective loads."
+(defvar persp-path-match-alist '()
+  "List of associations between perspective names and path-matching strings.
+When a file or directory is opened and its path matches with a value in this
+alist, the perspective for the corresponding key will be initialized.
+
+This alist may be modified manually, but is probably best left alone: use
+defpersp instead, which adds to this alist.")
+
+(defmacro defpersp (name path-match key
+                         &optional &key on-persp-init on-path-match)
+  "Define a perspective with a name NAME. Set up a keybinding to load the
+perspective manually by pressing `C-x p KEY`. Run ON-PERSP-INIT when the
+perspective loads and ON-PATH-MATCH whenever a file is opened which matches
+PATH_MATCH."
   (let* ((name-str (symbol-name name))
-        (funsym (intern (concat "persp/" name-str)))
-        (key (concat "C-x p " key)))
+         (funsym (intern (concat "persp/" name-str)))
+         (key (concat "C-x p " key)))
     ;; Define perspective trigger key
     `(progn
+       ,(if (stringp path-match)
+            `(add-to-list 'persp-path-match-alist
+                          '(,name-str . ,path-match)))
+
        (define-key persp-mode-map (kbd ,key) ',funsym)
 
        (defun ,funsym ()
          (interactive)
          (persp-switch ,name-str)
-         (unless (gethash ,name-str perspectives-hash)
-           ;; Perspective has not been activated in this session. Run the
-           ;; provided init code.
-           ,@body)))))
+
+         ,(if on-persp-init
+              `(unless (gethash ,name-str perspectives-hash)
+                ;; Perspective has not been activated in this session. Run the
+                ;; provided init code.
+                ,@on-persp-init))))))
 
 (defpersp irc nil "i"
   (erc))
