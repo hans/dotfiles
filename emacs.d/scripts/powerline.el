3d5c9054d50451081ead33681594ffec468739de
Hans Engel <engel@engel.uk.to>
2012-07-07 22:09:58 -0700

    Show perspective in powerline

--- emacs.d/scripts/powerline.el
+++ emacs.d/scripts/powerline.el
@@ -4,3 +4,30 @@
 (set-face-attribute 'mode-line nil
                     :background "grey18"
                     :box nil)
+
+(defpowerline perspective
+  (propertize
+   (if persp-curr
+       (concat "("
+               (substring (persp-name persp-curr) 0 1)
+               ")")
+     "")
+   'face (powerline-make-face color1)))
+
+(setq-default
+ mode-line-format
+ (list "%e"
+       '(:eval (concat
+                (powerline-rmw            'left   nil  )
+                (powerline-buffer-id      'left   nil  powerline-color1  )
+                (powerline-perspective    'left powerline-color1)
+                (powerline-major-mode     'left        powerline-color1  )
+                (powerline-minor-modes    'left        powerline-color1  )
+                (powerline-narrow         'left        powerline-color1  powerline-color2  )
+                (powerline-vc             'center                        powerline-color2  )
+                (powerline-make-fill                                     powerline-color2  )
+                (powerline-row            'right       powerline-color1  powerline-color2  )
+                (powerline-make-text      ":"          powerline-color1  )
+                (powerline-column         'right       powerline-color1  )
+                (powerline-percent        'right  nil  powerline-color1  )
+                (powerline-make-text      "  "    nil  )))))
