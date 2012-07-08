df171583a300da9c0e66043d092bf3cce1cfd373
Hans Engel <engel@engel.uk.to>
2012-07-07 20:26:50 -0700

    ERC config updates

--- emacs.d/scripts/erc.el
+++ emacs.d/scripts/erc.el
@@ -2,48 +2,51 @@
 
 (require 'erc)
 
-;; load modules
-(setq erc-modules '(autojoin button completion irccontrols list log match
-                             move-to-prompt netsplit networks noncommands
-                             readonly ring stamp track))
+(setq-default
+ erc-default-server "irc.freenode.net"
+ erc-nick "hansengel"
+ erc-user-full-name "Hans Engel"
 
-;; change prompt to >
-(setq erc-prompt ">")
+ ;; load modules
+ erc-modules '(autojoin button completion irccontrols list log match
+                        move-to-prompt netsplit networks noncommands
+                        readonly ring stamp track)
 
-;; auto-reconnect
-(setq erc-server-auto-reconnect t)
+ ;; change prompt to >
+ erc-prompt ">"
 
-;; track channels
-(erc-track-mode t)
+ ;; auto-reconnect
+ erc-server-auto-reconnect t
 
-;; don't track JOIN, NICK, PART, QUIT, or MODE
-(setq erc-track-exclude-types '("JOIN" "NICK" "PART" "QUIT" "MODE"
-                                "324" "329" "332" "333" "353" "457"))
+ ;; track channels
+ erc-track-mode t
 
-;; log
-(setq erc-log-channels t)
-(setq erc-log-channels-directory "~/.irclogs")
-(setq erc-save-buffer-on-part t) ;; automatically write to log files when parting channels
-(setq erc-hide-timestamps nil)
+ ;; don't track JOIN, NICK, PART, QUIT, or MODE
+ erc-track-exclude-types '("JOIN" "NICK" "PART" "QUIT" "MODE"
+                           "324" "329" "332" "333" "353" "457")
+
+ ;; log
+ erc-log-channels t
+ erc-log-channels-directory "~/.irclogs"
+ ;; automatically write to log files when parting channels
+ erc-save-buffer-on-part t
+ erc-hide-timestamps nil
+
+ ;; by default, when you join a channel ERC fills the screen with previous messages from a log for that channel if one exists.
+ ;; the following code disables that functionality. see http://www.emacswiki.org/emacs/ErcLogging#toc2
+ erc-log-insert-log-on-open nil)
 
 ;; auto-save log files on emacs exit
 (defadvice save-buffers-kill-emacs (before save-logs (arg) activate)
   (save-some-buffers t (lambda () (when (and (eq major-mode 'erc-mode)
                                              (not null buffer-file-name))))))
 
-;; by default, when you join a channel ERC fills the screen with previous messages from a log for that channel if one exists.
-;; the following code disables that functionality. see http://www.emacswiki.org/emacs/ErcLogging#toc2
-(setq erc-log-insert-log-on-open nil)
-
-;; resize ERC's wrap width when the window is resized
-(add-hook 'window-configuration-change-hook
-          '(lambda ()
-             (setq erc-fill-column (- (window-width) 2))))
-
 ;; fill the whole window
 (add-hook 'erc-mode-hook (lambda () (auto-fill-mode 1)))
 
 ;; show notifications (via notify.el) when my nick is mentioned
-(add-hook 'erc-text-matched-hook (lambda (match-type nick message)
-                                   (unless (posix-string-match "^\\** *Users on #" message)
-                                     (notify "ERC" (concat "You were mentioned in " (buffer-name (current-buffer)))))))
+(add-hook 'erc-text-matched-hook
+          (lambda (match-type nick message)
+            (unless (posix-string-match "^\\** *Users on #" message)
+              (notify "ERC" (concat "You were mentioned in "
+                                    (buffer-name (current-buffer)))))))
