77dc4298780b7f4a9a6703945be70a422ad3914f
Hans Engel <engel@engel.uk.to>
2012-07-13 21:44:38 -0700

    Get path-match hook working

--- emacs.d/scripts/perspective.el
+++ emacs.d/scripts/perspective.el
@@ -4,9 +4,16 @@
 (persp-mode 1)
 
 (defvar persp-path-match-alist '()
-  "List of associations between perspective names and path-matching strings.
+  "List of associations between perspective names and path-matching information.
 When a file or directory is opened and its path matches with a value in this
 alist, the perspective for the corresponding key will be initialized.
+
+Each value in the alist is a list, the first value of which is a string used to
+match paths and the second value of which is a lambda which is run when files
+which match the path are first opened.
+
+There is no need to include a call to open the perspective in said lambda: this
+is done automatically.
 
 This alist may be modified manually, but is probably best left alone: use
 defpersp instead, which adds to this alist.")
@@ -14,12 +21,17 @@ defpersp instead, which adds to this alist.")
 (defun persp-path-match (path)
     (cl-loop for (persp-name . path-match)
            in persp-path-match-alist
-           when (string-match-p path-match path)
+           when (string-match-p (car path-match) path)
 
            ;; Fetch the perspective by name
            do (progn
                 (persp-switch persp-name)
-                (let ((persp (gethash persp-name perspectives-hash)))
+                (let ((persp (gethash persp-name perspectives-hash))
+                      (on-match (cadr path-match)))
+                  ;; Run an on-match hook if it is set
+                  (if on-match
+                      (funcall on-match))
+
                   ;; Add the buffer to the fetched perspective's buffer list
                   (push (current-buffer) (persp-buffers persp))))))
 
@@ -28,8 +40,8 @@ defpersp instead, which adds to this alist.")
 (add-hook 'dired-after-readin-hook
           (lambda () (persp-path-match (dired-current-directory))))
 
-(defmacro defpersp (name path-match key
-                         &optional &key on-persp-init on-path-match)
+(defmacro* defpersp (name path-match key
+                          &optional &key on-persp-init on-path-match)
   "Define a perspective with a name NAME. Set up a keybinding to load the
 perspective manually by pressing `C-x p KEY`. Run ON-PERSP-INIT when the
 perspective loads and ON-PATH-MATCH whenever a file is opened which matches
@@ -41,7 +53,9 @@ PATH_MATCH."
     `(progn
        ,(if (stringp path-match)
             `(add-to-list 'persp-path-match-alist
-                          '(,name-str . ,path-match)))
+                          '(,name-str . (,path-match
+                                         (lambda ()
+                                           ,@on-path-match)))))
 
        (define-key persp-mode-map (kbd ,key) ',funsym)
 
