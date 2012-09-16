a642ed5376ad9e5cba4722f9357b99d4e8b3decd
Hans Engel <engel@engel.uk.to>
2012-09-16 14:50:04 -0700

    Org mode: Style HTML export

--- emacs.d/scripts/org.el
+++ emacs.d/scripts/org.el
@@ -23,7 +23,35 @@
       org-export-latex-minted-options '(("frame" "lines") ("fontsize" "\\footnotesize")
                                         ("linenos" ""))
 
-      org-babel-confirm-evaluate nil)
+      org-babel-confirm-evaluate nil
+
+      ;; Styled HTML export
+      org-export-html-style "<style type=\"text/css\">
+html, body { height: 100%; }
+body { background-color: #fafafa; font: 19px 'Baskerville'; color: #222;
+       line-height: 25px; }
+h1, h2, h3, h4, h5, h6 { color: #333; margin: 15px 0 0 0; font-weight: normal; }
+h1 { font-size: 36px; line-height: 46px; }
+h2 { font-size: 30px; line-height: 48px; }
+h3 { font-size: 24px; line-height: 32px; }
+dt { margin: 6px 0; }
+a { color: #840000; text-decoration: none; }
+a:hover { color: #aa0000; }
+pre, blockquote { border-left: 1px solid #ccc; margin: 0 0 20px 30px;
+                  padding-left: 20px; overflow: auto; }
+pre, tt, code { font-family: monospace; font-size: 17px; }
+sup { font-size: 14px; line-height: 10px; }
+#content { max-width: 650px; margin: 40px auto; padding-bottom: 20px;
+           border-bottom: 1px solid #ccc; }
+footer { margin: 100px auto; text-align: center; color: #666; font-size: 17px;
+         width: 650px; }
+footer a { color: #888; }
+footer a:hover { color: #aaa; }
+</style>"
+
+      ;; Be sure to set `org-export-html-postamble-format' so that the below
+      ;; setting is worth it!
+      org-export-html-postamble t)
 
 (define-key global-map (kbd "C-c c") 'org-capture)
 
