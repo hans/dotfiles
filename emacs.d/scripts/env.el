df430361dc48a5a044c775da8bff3014b4c736dd
Hans Engel <engel@engel.uk.to>
2012-09-16 14:50:28 -0700

    Use user bin directory at ~/.bin

--- emacs.d/scripts/env.el
+++ emacs.d/scripts/env.el
@@ -1,4 +1,4 @@
 ;;; Environment variables
 
-(setq exec-path (cons "/usr/local/bin" exec-path))
-(setenv "PATH" (concat "/usr/local/bin:" (getenv "PATH")))
+(setq exec-path (cons "~/.bin" (cons "/usr/local/bin" exec-path)))
+(setenv "PATH" (concat "~/.bin:/usr/local/bin:" (getenv "PATH")))
