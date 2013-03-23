6fd55a4152ce09970ecece9837e9a5f4fd2289b2
Hans Engel <engel@engel.uk.to>
2013-03-22 21:03:57 -0700

    nrepl!

--- emacs.d/scripts/clojure-mode.el
+++ emacs.d/scripts/clojure-mode.el
@@ -1,9 +1,6 @@
 ;; clojure-mode
 
-(setq swank-clojure-jar-path "~/.clojure/clojure.jar"
-      swank-clojure-extra-classpaths (list
-                                      "~/.clojure/clojure-contrib.jar"
-                                      "~/.clojure/src/clj/clojure"))
+(require 'nrepl)
 
-(eval-after-load "slime"
-  '(progn (slime-setup '(slime-repl))))
+(add-hook 'nrepl-mode-hook 'paredit-mode)
+(add-hook 'nrepl-interaction-mode-hook 'nrepl-turn-on-eldoc-mode)
