c34b93ba8362c8070d660707e47f37499d904a34
Hans Engel <engel@engel.uk.to>
2013-04-07 13:01:35 -0700

    Emacs compat fixes

--- emacs.d/scripts/color-theme.el
+++ emacs.d/scripts/color-theme.el
@@ -1,5 +1,16 @@
 ;;; Pretty colors
 
+(defun get-tuple-from-plist (the-plist)
+    (when the-plist
+      (cons (car the-plist) (cadr the-plist))))
+
+(defun plist-to-alist (the-plist)
+  (let (alist '())
+    (while the-plist
+      (add-to-list 'alist (get-tuple-from-plist the-plist))
+      (setq the-plist (cddr the-plist)))
+    alist))
+
 (require 'color-theme)
 
 (setq color-theme-is-global t)
