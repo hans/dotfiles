ba749118b98216664a8351f4fa47bf75c59eab5f
Hans Engel <engel@engel.uk.to>
2013-05-06 22:28:54 -0700

    Emacs: change default Linux font to DejaVu Sans-10

--- emacs.d/scripts/fonts.el
+++ emacs.d/scripts/fonts.el
@@ -2,11 +2,8 @@
 
 ;; From: http://community.schemewiki.org/cgi-bin/scheme.cgi?Emacs
 (and window-system
-     (create-fontset-from-fontset-spec
-      (concat
-       "-apple-monaco-medium-r-normal--12-*-*-*-*-*-fontset-monaco,"
-       "ascii:-apple-monaco-medium-r-normal--12-100-*-*-m-100-mac-roman"))
-     )
+   (add-to-list 'default-frame-alist
+                '(font . "DejaVu Sans Mono-10")))
 
 ;; To change a font size globally:
 ;; (set-face-attribute 'default nil :height 360)
