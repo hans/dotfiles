c3c52aeaa190e3f09ce500791b847d61408f8bff
Hans Engel <engel@engel.uk.to>
2013-07-06 12:30:48 -0700

    flycheck

--- emacs.d/scripts/flycheck.el
+++ emacs.d/scripts/flycheck.el
@@ -1,1 +1,12 @@
+;;; flycheck --- on-the-fly syntax checking
 
+;;; Commentary:
+
+;;; Code:
+
+(require 'flycheck)
+
+(add-hook 'after-init-hook #'global-flycheck-mode)
+
+(provide 'flycheck)
+;;; flycheck.el ends here
